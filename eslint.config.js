a2ede18351ee926957b5bfd0e78aca711333e41c
rbw <1813967922@qq.com>
2024-05-16 14:23:42 +0800

    init: first commit

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  vue: true,
+  javascript: true,
+  unocss: true,
+  rules: {
+    'no-undef': 'off',
+  },
+  formatters: {
+    /**
+     * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
+     * By default uses Prettier
+     */
+    css: true,
+    /**
+     * Format HTML files
+     * By default uses Prettier
+     */
+    html: true,
+  },
+})
